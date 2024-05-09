b26f2aa58ea553c2cd6efe696e9b5f7568112716
Bruno Rodrigo <bnorodrigo@gmail.com>
2024-05-09 17:28:03 +0100

    Variables

---
+++ fundamentals/variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var weight int
+	weight = 10
+	fmt.Println(weight)
+
+	var message string
+	message = "Hello"
+	fmt.Println(message)
+
+	message2 := "test"
+	fmt.Println(message2)
+
+	weight2 := 10.5
+	fmt.Println(weight2)
+}
